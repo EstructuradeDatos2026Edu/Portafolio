0503b892abab7f3fe4c4d444e560614948d22be5
1225100313jegh <1225100313.jegh@gmail.com>
2026-09-24 15:48:34 -0600

    mandando cambios de la recursividad

--- datos/Unidad1/recursividad/Recursividad.java
+++ datos/Unidad1/recursividad/Recursividad.java
@@ -1,16 +1,17 @@
 package datos.Unidad1.recursividad;
 
-public class Recursividad{
-	public static void saludo (int total){
-	
-		if(total <= 0)//condicion base
-			return;
-		else{
-			System.out.println("Hola");
-			saludo(total-1);
-		}
-	}
-	public static void main (String[] args){
-		saludo(10);
-}	
+public class Recursividad {
+    
+    public static void saludo(int total, String nombre) {
+        if (total <= 0) { // condición base
+            return;
+        } else {
+            System.out.println("Hola " + nombre);
+            saludo(total - 1, nombre); 
+        }
+    }
+
+    public static void main(String[] args) {
+        saludo(10, "Juan"); 
+    }
 }
