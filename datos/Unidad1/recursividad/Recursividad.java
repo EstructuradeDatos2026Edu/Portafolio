93d0e578cad1ab63d4235bc7f3b3ad7bb3dbb396
1225100313jegh <1225100313.jegh@gmail.com>
2026-09-25 14:47:31 -0600

    agregando suma de recursividad

--- datos/Unidad1/recursividad/Recursividad.java
+++ datos/Unidad1/recursividad/Recursividad.java
@@ -1,7 +1,7 @@
 package datos.Unidad1.recursividad;
 
 public class Recursividad {
-    
+
     public static void saludo(int total, String nombre) {
         if (total <= 0) { // condición base
             return;
@@ -10,18 +10,35 @@ public class Recursividad {
             saludo(total - 1, nombre); 
         }
     }
-	//Funcion que realiza una cuenta regresiva de acuerdo a un valor entero asignado Fecha:25/09/26 Autor:Juan González 
-    public static void cuentaRegresiva(int n){
-	if(n < 0) {
-		return;
-	}else{
-		System.out.println(n);
-		cuentaRegresiva(n-1);
-	}
-}
+
+    // Función que realiza una cuenta regresiva de acuerdo a un valor entero asignado Fecha:25/09/26 Autor:Juan González 
+    public static void cuentaRegresiva(int n) {
+        if (n < 0) {
+            return;
+        } else {
+            System.out.println(n);
+            cuentaRegresiva(n - 1);
+        }
+    }
+//suma todos los valores dentro del arreglo
+    public static int sumaRecursiva(int[] datos, int tam) {
+        if (tam <= 0) {
+            return 0;
+        } 
+        // Paso Recursivo: Acumula el elemento actual y llama con la longitud restante
+        else {
+            return datos[tam - 1] + sumaRecursiva(datos, tam - 1);
+        }
+    }
 
     public static void main(String[] args) {
         //saludo(100, "Juan");
-	cuentaRegresiva(100); 
+        //cuentaRegresiva(100);
+        int[] datos = {5, 10, 15, 20}; 
+
+        // Pasamos el arreglo y su tamaño total
+        int resultado = sumaRecursiva(datos, datos.length);
+
+        System.out.println("La suma de los valores del arreglo es: " + resultado);
     }
 }
